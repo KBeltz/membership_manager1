de2eeaccf0dceb1cc83e7d2a0b3c0071ece1d3ea
Kristin Beltz <beltz08@me.com>
2016-12-04 11:12:55 -0600

    Remove references to user

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,11 @@ Rails.application.routes.draw do
   resources :belt_levels
   resources :membership_types
   resources :login_attempts
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
