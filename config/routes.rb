0d3a00dd797a2a7bfcb3d233695eb00444ec2864
Kristin Beltz <beltz08@me.com>
2016-12-04 10:24:50 -0600

    Drop users table, generate active admin installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :memberships
   resources :guardians
   resources :belts
